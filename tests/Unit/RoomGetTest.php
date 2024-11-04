e7cfd5eea74c1f57401f9380ed6f15c0bda77116
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2024-11-04 10:51:08 -0300

    test: Adiciona RoomGetTest com PHPUnit

---
+++ tests/Unit/RoomGetTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Hotel;
+use Tests\TestCase;
+use App\Models\Room;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RoomGetTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testGetRooms()
+    {
+        $hotel = Hotel::factory()->create();
+        $room = Room::factory()->create();
+
+        $response = $this->get('/api/rooms');
+
+        $response->assertStatus(200);
+
+        $response->assertJsonFragment([
+            'hotel_id'=>$room->hotel_id,
+            'room_id' => $room->room_id,
+            'name' => $room->name,
+        ]);
+
+        $hotel->delete();
+        $room->delete();
+    }
+}
